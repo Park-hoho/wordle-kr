2516a940368468265dd5b85412158a8b27b57ac3
ByungHo <qudgh4290@gmail.com>
2022-02-07 23:53:37 +0900

    Add Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,26 @@
 import React, {useState} from 'react';
 import styled from "styled-components";
 
-const Popup = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.7);
-`;
-
 function Header() {
   const [isStatusPopup, setIsStatusPopup] = useState(false);
 
+  const handleMenu = () => {
+    console.log("menu");
+  }
+  
   return (
     <>
-      <header>
+      <HeaderTag>
         <h1>WORDLE KR</h1>
-        <button onClick={() => setIsStatusPopup(true)}>팝업</button>
-      </header>
+        <div>
+          <button onClick={() => setIsStatusPopup(true)}>팝업</button>
+          <MenuIcon onClick={handleMenu}>
+            <span />
+            <span />
+            <span />
+          </MenuIcon>
+        </div>
+      </HeaderTag>
 
       {
         isStatusPopup && (
@@ -44,3 +43,42 @@ function Header() {
 }
 
 export default Header;
+
+const Popup = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.7);
+`;
+
+const HeaderTag = styled.header`
+  padding: 0 0 0 16px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: black;
+  border-bottom: 1px solid #e1e1e1;
+`;
+
+const MenuIcon = styled.button`
+  width: 60px;
+  height: 60px;
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+  span {
+    width: 24px;
+    height: 2px;
+    display: block;
+    margin: 4px auto;
+    transition: all 0.3s ease-in-out;
+    background-color: #000000;
+  }
+`;
